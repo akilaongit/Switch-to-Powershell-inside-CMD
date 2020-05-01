f9fbda2a732f32bd67bfd1ceeaff9725bfab989d
AkilaBandara <59506472+akilaongit@users.noreply.github.com>
2020-05-01 17:24:18 +0530

    Add files via upload

---
+++ l0calpr0mptsys.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "[i]Author: Akila Bandara\n";
+	cout << "[i]Follow: twitter.com/akilaontweet\n";
+	cout << "[i]Web: www.akilabandara.rf.gd\n";
+	system("C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe");
+
+}
